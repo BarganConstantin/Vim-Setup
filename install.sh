d6778a5db4d02e7d4191c6381b5a5573b7cef742
kostea123 <60443226+kostea123@users.noreply.github.com>
2022-01-15 20:22:17 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,37 @@
+# check if directory exists
+if [ -d /etc/vim/ ]; then
+  echo "directory /etc/vim/ exist"
+ else
+  echo "directory /etc/vim/ not exist"
+  mkdir /etc/"vim"/ 
+  echo "create /etc/vim/"
+
+fi
+
+touch vimrc.local
+
+echo “set number” >> vimrc.local
+echo “colo monokai” >> vimrc.local
+echo “set tabstop=4” >> vimrc.local
+echo “set shiftwidth=4” >> vimrc.local
+echo “set softtabstop=4” >> vimrc.local
+echo “set expandtab” >> vimrc.local
+echo “filetype plugin indent on” >> vimrc.local
+echo “set autoindent” >> vimrc.local
+echo “set laststatus=2” >> vimrc.local
+echo “let g:airline#extensions#tabline#enabled = 1” >> vimrc.local
+echo “let g:airline_theme='base16_greenscreen'” >> vimrc.local
+
+mv vimrc.local /etc/vim/
+
+mkdir -p ~/.vim/pack/vendor/start/ 
+
+git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/pack/vendor/start
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/vendor/start
+git clone https://github.com/alvan/vim-closetag.git ~/.vim/pack/vendor/start
+git clone https://github.com/sheerun/vim-polyglot.git ~/.vim/pack/vendor/start
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/pack/vendor/start
+git clone https://github.com/sickill/vim-monokai.git ~/.vim/pack/vendor/start
+git clone https://github.com/vim-scripts/AutoComplPop.git ~/.vim/pack/vendor/start
+
