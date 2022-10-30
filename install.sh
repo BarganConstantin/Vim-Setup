cbc988aa4f049f958e55cb387f7569cc6bbf4741
Bargan Constantin <60443226+BarganConstantin@users.noreply.github.com>
2022-10-30 21:54:08 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ echo   "        ▌ ▌▗      ▞▀▖   ▐
         ▚▗▘▄ ▛▚▀▖ ▚▄ ▞▀▖▜▀ ▌ ▌▛▀▖
         ▝▞ ▐ ▌▐ ▌ ▖ ▌▛▀ ▐ ▖▌ ▌▙▄▘
          ▘ ▀▘▘▝ ▘ ▝▀ ▝▀▘ ▀ ▝▀▘▌"
-if [ -d /etc/vim/ ]; then
+if [ -d /etc/vim/ ]
+ then
   echo "directory /etc/vim/ exist"
  else
   echo "directory /etc/vim/ not exist"
@@ -12,7 +13,7 @@ if [ -d /etc/vim/ ]; then
 
 fi
 
-touch vimrc.local
+touch vimrc
 
 echo set number >> vimrc
 echo colo monokai >> vimrc
@@ -26,45 +27,61 @@ echo set laststatus=2 >> vimrc
 echo let g:airline#extensions#tabline#enabled = 1 >> vimrc
 echo let g:airline_theme="'base16_greenscreen'" >> vimrc
 
-mv vimrc.local /etc/vim/
+mv vimrc /etc/vim/
 
-mkdir -p ~/.vim/pack/vendor/start/
-mkdir plugin
+echo "------> $HOME/.vim/pack/vendor/start" 
+
+home=$( echo $PWD | cut -d'/' -f -3 )
+path_plugin="${home}/.vim/pack/vendor/start"
+echo $path_plugin
+
+
+if [ -d $path_plugin ]
+  then
+  echo "directory for plugin exist"
+  else
+  echo "directory for plugin not exist, try to create"
+  mkdir -p $path_plugin
+  if [ -d $path_plugin ]
+  then
+      echo "directory for plugin created"
+  else
+      echo "Error: directory for plugin not created"
+      exit 1
+  fi
+fi
 
 git clone https://github.com/jiangmiao/auto-pairs.git
-cp -r auto-pairs plugin
+cp -r auto-pairs $path_plugin
 rm -r auto-pairs
 
 git clone https://github.com/vim-airline/vim-airline.git
-cp -r vim-airline plugin
+cp -r vim-airline $path_plugin
 rm -r vim-airline
 
 git clone https://github.com/alvan/vim-closetag.git
-cp -r vim-closetag plugin
+cp -r vim-closetag $path_plugin
 rm -r vim-closetag
 
 git clone https://github.com/sheerun/vim-polyglot.git
-cp -r vim-polyglot plugin
+cp -r vim-polyglot $path_plugin
 rm -r vim-polyglot
 
 git clone https://github.com/preservim/nerdtree.git
-cp -r nerdtree plugin
+cp -r nerdtree $path_plugin
 rm -r nerdtree
 
 git clone https://github.com/vim-airline/vim-airline-themes.git
-cp -r vim-airline-themes plugin
+cp -r vim-airline-themes $path_plugin
 rm -r vim-airline-themes
 
 git clone https://github.com/sickill/vim-monokai.git
-cp -r vim-monokai plugin
+cp -r vim-monokai $path_plugin
 rm -r vim-monokai
 
 git clone https://github.com/vim-scripts/AutoComplPop.git
-cp -r AutoComplPop plugin
+cp -r AutoComplPop $path_plugin
 rm -r AutoComplPop
-
-cp -r plugin/* ~/.vim/pack/vendor/start/
-rm -r plugin
 
 echo -e "\t -------------------------
 \t/     Congratulation!     \\
